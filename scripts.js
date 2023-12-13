cb95967284b540211ddfbb27444d0cf237deb360
camylla14 <vcamylla222@gmail.com>
2023-12-13 18:31:51 -0300

    Projeto Commit - Projeto iphone

---
+++ scripts.js
@@ -0,0 +1,10 @@
+
+function trocaCor(cor) {
+    let circulo = document.querySelector(".circulo")
+    circulo.style.background = cor
+}
+
+function trocaImagem(imagem) {
+    let imgIphone = document.querySelector(".iphone")
+    imgIphone.src = imagem
+}
